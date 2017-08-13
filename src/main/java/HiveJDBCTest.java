4876bc019c69f0ff3daeea8ee8550ef8ada42de7
renshunyu <376180929@qq.com>
2017-08-13 11:20:26 +0800

    Initial commit

---
+++ src/main/java/HiveJDBCTest.java
@@ -0,0 +1,64 @@
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.DriverManager;
+
+public class HiveJDBCTest {
+
+    private static String driverName = "org.apache.hive.jdbc.HiveDriver";
+
+    public static void main(String[] args)
+            throws SQLException {
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        Connection con = DriverManager.getConnection(
+                "jdbc:hive2://10.1.198.54:10000/aiiap", "clouderauser", "clouderauser");
+        Statement stmt = con.createStatement();
+        String tableName = "jdbcTest";
+        stmt.execute("drop table if exists " + tableName);
+        stmt.execute("create table " + tableName +
+                " (key int, value string)");
+        System.out.println("Create table success!");
+        // show tables
+        String sql = "show tables '" + tableName + "'";
+        System.out.println("Running: " + sql);
+        ResultSet res = stmt.executeQuery(sql);
+        if (res.next()) {
+            System.out.println(res.getString(1));
+        }
+
+        // describe table
+        sql = "describe " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(res.getString(1) + "\t" + res.getString(2));
+        }
+
+
+        sql = "select * from " + tableName;
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(String.valueOf(res.getInt(1)) + "\t"
+                    + res.getString(2));
+        }
+
+        sql = "select count(1) from " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(res.getString(1));
+        }
+        stmt.execute("drop table jdbctest");
+        res = stmt.executeQuery("show tables");
+        while (res.next()) {
+            System.out.println(res.getString(1));
+        }
+    }
+}
